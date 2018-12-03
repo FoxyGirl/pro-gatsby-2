749664bcf9f75638238a43fdc62ee89150fae655
Elena Pavlova <lenlho22@gmail.com>
2018-12-03 11:03:15 +0300

    Added createPages in gatsby-node

---
+++ src/components/PostLayout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Layout from './layout'
+
+class PostLayout extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>Posts</h1>
+      </Layout>
+    )
+  }
+}
+
+export default PostLayout
