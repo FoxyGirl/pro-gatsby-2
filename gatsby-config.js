130e16b0f49c68370cae2892c53d787c6eb259f3
Elena Pavlova <lenlho22@gmail.com>
2018-12-12 14:59:58 +0300

    Added offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
     description: 'New super blog!',
+    siteUrl: 'https://sad-raman-5a6914.netlify.com',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -25,20 +26,21 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'pro-Gatsby2',
+        short_name: 'pro-Gatsby2',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/phoenix-seeklogo_2.svg', // This path is relative to the root of the site.
       },
     },
     'gatsby-transformer-remark',
     'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
     'gatsby-plugin-netlify',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
 }
